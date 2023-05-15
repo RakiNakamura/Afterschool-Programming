33fc1bdd5f1ab2d7451bb0adf8db5fb337076798
RakiNakamura <133629360+RakiNakamura@users.noreply.github.com>
2023-05-15 15:30:01 +0900

    First commit

---
+++ hello would.py
@@ -0,0 +1,10 @@
+Python 3.9.4 (tags/v3.9.4:1f2e308, Apr  4 2021, 13:27:16) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> message="hello would"
+>>> message2=12
+>>> message3=78.8989891
+>>> message4=True
+>>> 
+>>> print(message)
+hello would
+>>>  print(message)
